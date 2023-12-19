c51fe4e565d2bb011adec69f0a61c8fb2cda0bbb
AndreiTeodorPop <andrei_teodor.pop@yahoo.com>
2023-12-19 16:40:56 +0200

    add hook class and split tests

---
+++ src/test/java/org/example/testng/pages/DownloadPage.java
@@ -0,0 +1,36 @@
+package org.example.testng.pages;
+
+import com.microsoft.playwright.Download;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import org.example.Hook;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class DownloadPage extends Hook {
+
+    @Test
+    public void downloadFileDemo() {
+        page.navigate("https://demo.automationtesting.in/Index.html");
+        page.locator("//img[@id='enterimg']").click();
+        page.locator("(//p[@class='fc-button-label'])[1]").getByText("Consent").click();
+        Locator moreList = page.locator("//a[@class='dropdown-toggle']").getByText("More");
+        moreList.click();
+        Locator fileDownload = page.locator("ul.dropdown-menu li", new Page.LocatorOptions().setHasText("File Download"));
+        fileDownload.click();
+        assertThat(page).hasTitle("File input - Multi select");
+        Download download = page.waitForDownload(() -> {
+            page.locator("//a[@class='btn btn-primary']").click();
+        });
+        System.out.println(download.path());
+        String path = System.getProperty("user.dir");
+        download.saveAs(Paths.get(path + File.separator + "src/main/resources/" + "SampleFile.pdf"));
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("target/demo-screenshots/DownloadPage.png")).setFullPage(true));
+        page.click("//a[contains(@href,'Index.html')]");
+    }
+
+}
---
+++ src/test/java/org/example/testng/pages/RegisterPage.java
@@ -0,0 +1,28 @@
+package org.example.testng.pages;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import org.example.Hook;
+import org.testng.annotations.Test;
+
+import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class RegisterPage extends Hook {
+
+    @Test
+    public void registerDemo() {
+        page.navigate("https://demo.automationtesting.in/Index.html");
+        page.locator("//img[@id='enterimg']").click();
+        assertThat(page).hasTitle("Register");
+        page.locator("(//p[@class='fc-button-label'])[1]").getByText("Consent").click();
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("target/demo-screenshots/RegisterPage.png")).setFullPage(true));
+        page.locator("//input[@ng-model='FirstName']").fill("Andrei");
+        page.locator("//input[@ng-model='LastName']").fill("Pop");
+        page.locator("//button[@id='Button1']").screenshot(new Locator.ScreenshotOptions().setPath(Paths.get("target/demo-screenshots/RefreshButton.png")));
+        page.click("//button[@id='Button1']");
+        page.click("//a[contains(@href,'Index.html')]");
+    }
+
+}
