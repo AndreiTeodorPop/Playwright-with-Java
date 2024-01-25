21f28f57649e4cc0f4ee86dd172911d37e53d58d
AndreiTeodorPop <andrei_teodor.pop@yahoo.com>
2024-01-25 23:36:37 +0200

    delete string

--- src/test/java/org/playwright/elements/WebTablePageElements.java
+++ src/test/java/org/playwright/elements/WebTablePageElements.java
@@ -10,7 +10,6 @@ public class WebTablePageElements {
     public static final String firstNameBox = "//input[@ng-model='FirstName']";
     public static final String lastNameBox = "//input[@ng-model='LastName']";
     public static final String emailBox = "//input[@type='email']";
-    public static final String radioButtons = "#radiooptions";
     public static final String refreshButton = "//button[@id='Button1']";
     public static final String moreList = "//a[@class='dropdown-toggle']";
     public static final String fileDownloadPage = "ul.dropdown-menu li";
